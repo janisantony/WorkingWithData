74b31236c84bfb03199d99013052660d3586211f
janisantony <120107676+janisantony@users.noreply.github.com>
2022-12-15 16:08:41 +0400

    Add files via upload

---
+++ EmployeeWithClassLibrary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EmployeeDemo;
+
+namespace EmployeeWithClassLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeRepo = new EmployeeRepository();
+            employeeRepo.InsertEmployees(new Employee
+            {
+                Name = "Mariam",
+                Age = 30
+            });
+
+            employeeRepo.UpdateEmployee(new Employee
+            {
+                Id = 4,
+                Name = "Teny",
+                Age = 30
+            });
+
+            employeeRepo.DeleteEmployee(5);
+            var employeesList = employeeRepo.GetEmployees();
+            foreach (var employee in employeesList)
+            {
+                Console.WriteLine($"{employee.Name}---{employee.Id}-----{employee.Age}");
+            }
+
+        }
+    }
+}
